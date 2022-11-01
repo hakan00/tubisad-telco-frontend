61feadb1b48a19e8af78aaa3b266f1e40de5c281
hakan özer <hakan.ozer@codemine.com.tr>
2022-11-01 18:55:07 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListviewComponent } from './components/listview/listview.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [AppComponent, CalculatorComponent, ListviewComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
